32929874944af2795e671f8d4bc7a232b4ae8ee6
harisw <harrysw6@gmail.com>
2024-12-29 23:35:12 +0900

    chore/Used env variable for DB

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -4,20 +4,26 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "hari123"
-	dbname   = "wenakk"
-)
-
 func Connect() *sql.DB {
-	connInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	_ = godotenv.Load()
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	portInt, err := strconv.Atoi(port)
+	if err != nil {
+		panic(err)
+	}
+	connInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, portInt, user, password, dbname)
 
 	db, err := sql.Open("postgres", connInfo)
 	if err != nil {
